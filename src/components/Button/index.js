35556949439a71f078c24e7915755a2afbc59298
Beka Lobzhanidze <beka_lobzhanidze@epam.com>
2023-03-26 16:05:07 +0400

    feat: add webdriverio configuration and one simple test for searchcomponent

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,7 @@ import styles from './button.module.scss';
 export default function Button({btnClass, text , ...props}) {
     return (
         <div className={styles.button}>
-            <button className={btnClass === 'primary' ? styles.button__primary : styles.button__secondary} {...props}>{text}</button>
+            <button type='submit' className={btnClass === 'primary' ? styles.button__primary : styles.button__secondary} {...props}>{text}</button>
         </div>
     )
 }
